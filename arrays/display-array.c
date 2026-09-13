c167e05044d25648cd811d8d2879930713fd9b35
Reyaz Ali <reyazali.dev@gmail.com>
2026-09-13 22:37:13 +0530

    Add c program to display elements of an array

--- arrays/display-array.c
+++ arrays/display-array.c
@@ -1,6 +1,6 @@
 //Purpose: Reading Size of Array and Elements from User, Then Displaying Elements of an Array.
 //Author: Reyaz Ali
-//Date: Sun 13 Sep
+//Date: Sun 13 Sep 2026
 //Time: 10:30 PM
 //Email: <reyazali.dev@gmail.com>
 //Github: <https://www.github.com/reyazali-dev>
