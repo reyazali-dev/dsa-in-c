f6f00c2fa0c06f0e3faf6326871d4c391f0a06e2
Reyaz Ali <reyazali.dev@gmail.com>
2026-09-13 22:32:21 +0530

    Add c program to display elements of an array

--- arrays/display-array.c
+++ arrays/display-array.c
@@ -1,7 +1,9 @@
 //Purpose: Reading Size of Array and Elements from User, Then Displaying Elements of an Array.
 //Author: Reyaz Ali
 //Date: Sun 13 Sep
-//Time: 9:40 PM
+//Time: 10:30 PM
+//Email: <reyazali.dev@gmail.com>
+//Github: <https://www.github.com/reyazali-dev>
 #include<stdio.h>   //This line includes the information about C Standard Library
 int main(void) { // Defining Main, it calls other functions to perform their jobs wheather you wrote it or it is a library function.
     //The Execution of Program Begins Here
