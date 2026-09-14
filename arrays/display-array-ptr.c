80955b25a1da32a63157a33f2d74682f1c602ef5
Reyaz Ali <reyazali.dev@gmail.com>
2026-09-14 22:30:19 +0530

    Added a c program that displays elements of an array using pointers

---
+++ arrays/display-array-ptr.c
@@ -0,0 +1,30 @@
+//Purpose: Display Elements of an Array Using Pointers.
+//Author: Reyaz Ali
+
+#include<stdio.h>
+int main(void) {
+    start:
+    int n,i;
+    int *p; //Pointer to an integer.
+    printf("\nEnter the Size of Array: ");
+    scanf("%d",&n);
+    if (n>0) {
+        int arr[n];
+        p=arr; // p stores base address of the array
+        printf("\nNow Enter Elements: ");
+        for (i=0;i<n;i++) {
+            scanf("%d",&p[i]);
+        }
+        printf("\nThe Elements are: ");
+        for (i=0;i<n;i++) {
+            printf("%d\t",p[i]);
+        }
+    }
+    else {
+        printf("\nEnter a Valid Size.");
+        goto start;
+    }
+
+
+    return 0;
+}
